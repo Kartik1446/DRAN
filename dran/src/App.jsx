5db271aeb8f37d944bc0eec8bbaefda161387fc8
Kartik <kartik140406@gmail.com>
2025-10-16 22:30:02 +0530

    dc

--- dran/src/App.jsx
+++ dran/src/App.jsx
@@ -10,16 +10,71 @@ function App() {
                     <input type="datetime-local" />
                 </div>
                 <div className='description'>
-                    <input type="text" placeholder="Enter amount" />
+                    <input type="text" placeholder="Description" />
                 </div>
                 <button> Add new Transsaction</button>
             </form>
             <div className='transactions'>
                 <div className='transaction'>
-                    <p>+200 new samsung tv</p>
-                    <p>2023-01-01 12:00:00</p>
-                    <p>$200.00</p>
+                    <div>
+                        <div className='left'>
+                            <div className='name'>New Samsung TV</div>
+                            <div className='description'>It was time for a new TV</div>
+                        </div>
+                        <div className='right'>
+                            <div className='price'>$500.00</div>
+                            <div className='datetime'>2022-12-18 15:45</div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+                <div className='transaction'>
+                    <div>
+                        <div className='left'>
+                            <div className='name'>New Samsung TV</div>
+                            <div className='description'>It was time for a new TV</div>
+                        </div>
+                        <div className='right'>
+                            <div className='price'>$500.00</div>
+                            <div className='datetime'>2022-12-18 15:45</div>
+                        </div>
+                </div>
+            </div>
+                <div className='transaction'>
+                    <div>
+                        <div className='left'>
+                            <div className='name'>New Samsung TV</div>
+                            <div className='description'>It was time for a new TV</div>
+                        </div>
+                        <div className='right'>
+                            <div className='price'>$500.00</div>
+                            <div className='datetime'>2022-12-18 15:45</div>
+                        </div>
+                </div>        
+            </div>
+                <div className='transaction'>
+                    <div>
+                        <div className='left'>
+                            <div className='name'>New Samsung TV</div>
+                            <div className='description'>It was time for a new TV</div>
+                        </div>
+                        <div className='right'>
+                            <div className='price'>$500.00</div>
+                            <div className='datetime'>2022-12-18 15:45</div>
+                        </div>
+                </div>
+            </div>
+                <div className='transaction'>
+                    <div>
+                        <div className='left'>
+                            <div className='name'>New Samsung TV</div>
+                            <div className='description'>It was time for a new TV</div>
+                        </div>
+                        <div className='right'>
+                            <div className='price'>$500.00</div>
+                            <div className='datetime'>2022-12-18 15:45</div>
+                        </div>
+            </div>
             </div>
         </main>
     );
